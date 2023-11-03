87a04174f17fc285328f8e7487cebce7c2dbcdbc
Mehmet Emin Ak <mak44162@gmail.com>
2023-11-03 10:58:35 +0300

    NavigationController added

--- PokemonAPI/Helpers/SceneDelegate.swift
+++ PokemonAPI/Helpers/SceneDelegate.swift
@@ -20,7 +20,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = HomeViewController()
+        
+        let homeVC = HomeViewController()
+        let navigationController = UINavigationController(rootViewController: homeVC)
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 
--- PokemonAPI/View/HomeViewController.swift
+++ PokemonAPI/View/HomeViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private var searchPokemonBar : UISearchBar = {
+        let searchBar = UISearchBar()
+        return searchBar
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
