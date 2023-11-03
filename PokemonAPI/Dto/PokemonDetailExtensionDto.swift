d186bf07a93196bfb6845064dd7d35dfd2ca6edc
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2023-11-03 21:48:15 +0300

    Added PokemonDetailExtensionDto

---
+++ PokemonAPI/Dto/PokemonDetailExtensionDto.swift
@@ -0,0 +1,24 @@
+//
+//  SpeciesDto.swift
+//  PokemonAPI
+//
+//  Created by Ali Çolak on 3.11.2023.
+//
+
+import Foundation
+import UIKit
+
+struct PokemonDetailExtensionDto {
+    
+    var pokemonDetailDto: PokemonDetailDto
+    var descripiton : String?
+    var color : UIColor?
+    
+    init(pokemonDetailDto: PokemonDetailDto, descripiton: String? = nil, color: UIColor? = nil) {
+        self.pokemonDetailDto = pokemonDetailDto
+        self.descripiton = descripiton
+        self.color = color
+    }
+    
+}
+
