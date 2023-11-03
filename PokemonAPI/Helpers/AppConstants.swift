816254bc8886a64fca450c784b95656dd457e631
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2023-11-03 14:44:53 +0300

    Add app constants

---
+++ PokemonAPI/Helpers/AppConstants.swift
@@ -0,0 +1,18 @@
+//
+//  AppConstants.swift
+//  PokemonAPI
+//
+//  Created by Ali Çolak on 3.11.2023.
+//
+
+import Foundation
+
+struct AppConstants {
+    static let allPokemonCount = 1292
+    static let limitOWnImageId = 10263
+    static let limitOwnSpeciesId = 1017
+    static let pageSize = 21
+    static let baseUrl = "https://pokeapi.co/api/v2/"
+    static let noImageUrl = "https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg"
+    static let baseImageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
+}
